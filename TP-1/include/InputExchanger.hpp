96d742123c5b78da29a562d96c7fc1108af299d3
Erik Neves <66576214+erikneves04@users.noreply.github.com>
2023-10-11 10:42:33 -0300

    fix input exchanger

--- TP-1/include/InputExchanger.hpp
+++ TP-1/include/InputExchanger.hpp
@@ -9,21 +9,17 @@
 #include "DataStructures/Queue.hpp"
 #include "DataStructures/LinkedList.hpp"
 
-#define EXISTS ("e")
-#define FOR_ALL ("a")
-
 class InputExchanger
 {
     private:
         std::string _input;
         LinkedList<int> _variablesIndex;
 
-        void SetupVariablesIndex();
         bool HasVariableForIndex(int index);
         LinkedList<std::string> GenerateBinaryCombinations(int bitsCount);
     public:
-        InputExchanger(std::string input);
-        Queue<LinkedList<InputValue>*> GetAllInputsCombination();
+        InputExchanger(std::string input, LinkedList<int> variablesIndex);
+        Queue<LinkedList<InputValue>*>* GetAllInputsCombination();
 };
 
 #endif
--- TP-1/src/InputExchanger.cpp
+++ TP-1/src/InputExchanger.cpp
@@ -6,56 +6,10 @@
 #include "DataStructures/Queue.hpp"
 #include "DataStructures/LinkedList.hpp"
 
-InputExchanger::InputExchanger(std::string input)
+InputExchanger::InputExchanger(std::string input, LinkedList<int> variablesIndex)
 {
     _input = input;
-    SetupVariablesIndex();
-}
-
-Queue<LinkedList<InputValue>*> InputExchanger::GetAllInputsCombination()
-{
-    Queue<LinkedList<InputValue>*> queue;
-    LinkedList<std::string> combinations = GenerateBinaryCombinations(_variablesIndex.Length());
-
-    for(int i = 0; i < combinations.Length(); i++)
-    {
-        LinkedList<InputValue>* input = new LinkedList<InputValue>();
-        
-        int currentCombinationIndex = 0;
-        std::string currentCombination = combinations.Get(i);
-
-        for(int j = 0; j < _input.length(); j++)
-        {
-            if (HasVariableForIndex(j))
-            {
-                std::string index = std::to_string(j);
-                std::string value(1, currentCombination[currentCombinationIndex]);
-                input->Insert(InputValue(index, value == "1"));
-
-                currentCombinationIndex++;
-            }
-            else
-            {
-                std::string index = std::to_string(i);
-                std::string value(1, _input[j]);
-
-                input->Insert(InputValue(index, value == "1"));
-            }
-        }
-        queue.Insert(input);
-    }
-    
-    return queue;
-}
-
-void InputExchanger::SetupVariablesIndex()
-{
-    for (int i = 0; i < _input.length(); i++)
-    {
-        std::string input(1, _input[i]);
-        if (input == EXISTS || input == FOR_ALL)
-            _variablesIndex.Insert(i);
-    } 
+    _variablesIndex = variablesIndex;
 }
 
 bool InputExchanger::HasVariableForIndex(int index)
@@ -67,6 +21,41 @@ bool InputExchanger::HasVariableForIndex(int index)
     }
 
     return false;
+}
+
+Queue<LinkedList<InputValue>*>* InputExchanger::GetAllInputsCombination()
+{
+    Queue<LinkedList<InputValue>*>* queue = new Queue<LinkedList<InputValue>*>();
+    LinkedList<std::string> combinations = GenerateBinaryCombinations(_variablesIndex.Length());
+
+    for(int i = 0; i < combinations.Length(); i++)
+    {
+        LinkedList<InputValue>* input = new LinkedList<InputValue>();
+        
+        int currentCombinationIndex = 0;
+        std::string currentCombination = combinations.Get(i);
+        for(size_t j = 0; j < _input.length(); j++)
+        {
+            if (HasVariableForIndex(j))
+            {
+                std::string index = std::to_string(j);
+                std::string value(1, currentCombination[currentCombinationIndex]);
+                input->Insert(InputValue(index, value == "1"));
+
+                currentCombinationIndex++;
+            }
+            else
+            {
+                std::string index = std::to_string(j);
+                std::string value(1, _input[j]);
+
+                input->Insert(InputValue(index, value == "1"));
+            }
+        }
+        queue->Insert(input);
+    }
+    
+    return queue;
 }
 
 LinkedList<std::string> InputExchanger::GenerateBinaryCombinations(int bitsCount) 
