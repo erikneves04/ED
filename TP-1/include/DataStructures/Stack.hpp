7a9e7b99ee28ca3469960f2d0c185df3e33d9da2
Erik Neves <66576214+erikneves04@users.noreply.github.com>
2023-10-21 16:35:03 -0300

    Tp 1 (#2)
    * Create LinkedList, Queue and Stack
    
    * nodule types
    
    * set vccode settings
    
    * access stack top
    
    * create ExpressionOrderer
    
    * list validation
    
    * change nodules
    
    * create expression
    
    * fix memory leaks
    
    * double NOT fix
    
    * remove expression vector
    
    * improve orderer
    
    * create input exchanger
    
    * disableValuesMemoryDelete
    
    * fix input exchanger
    
    * create SatisfactionEvaluator
    
    * list nullptr validation
    
    * fix space
    
    * main menu
    
    * delete unused methods
    
    * Update SatisfactionEvaluator.cpp
    
    * fix list get
    
    * change input exchanger to pointer
    
    * change vars and expression to pointers
    
    * fix expression parse
    
    * add list contains
    
    * print satisfaction solution
    
    * change exe name
    
    * concat operation nodules
    
    * irrelevant define
    
    * var count
    
    * fix operation execute
    
    * move main
    
    * fix IsVariableIrreleant
    
    * fix forall assert
    
    * add fails list
    
    * fix var required avaliation
    
    * memory free fix
    
    * add tests
    
    * Update Expression.cpp
    
    * remove unused vars
    
    * fix assert evaluation
    
    * improve solutions access
    
    * improve compare
    
    * Update SatisfactionEvaluator.cpp
    
    * improve list access
    
    * Update .gitignore
    
    * add seed
    
    * refact variable count
    
    * fix variable assign
    
    * data structures documentation
    
    * expression documentation
    
    * LinkedList private method documentation
    
    * input exchanger documentation
    
    * nodules documentation
    
    * remove extra lines
    
    * satisfaction evaluator documentation
    
    * DeleteIfIsResultNodule documentation
    
    * add ExecuteInputCombination
    
    * add main documentation
    
    * fix evaluator reference

--- TP-1/include/DataStructures/Stack.hpp
+++ TP-1/include/DataStructures/Stack.hpp
@@ -5,6 +5,16 @@
 
 #include "Node.hpp"
 
+/**
+ * @file Stack.hpp
+ * @brief Classe que representa uma pilha (stack) em C++.
+ *
+ * Esta classe implementa uma estrutura de dados de pilha, onde os elementos são inseridos e
+ * removidos do topo da pilha (LIFO - Last-In, First-Out). A pilha permite inserção, remoção,
+ * acesso ao elemento no topo e verificação se está vazia.
+ *
+ * @tparam DataType O tipo de dados dos elementos da pilha.
+ */
 template <class DataType> 
 class Stack
 {
@@ -15,6 +25,11 @@ class Stack
         int _size;
 
     public:
+        /**
+         * @brief Construtor padrão da classe Stack.
+         *
+         * Cria uma nova instância de pilha vazia.
+         */
         Stack()
         {
             _first = nullptr;
@@ -22,6 +37,11 @@ class Stack
             _size = 0;
         };
 
+        /**
+         * @brief Destrutor da classe Stack.
+         *
+         * Libera a memória alocada para os elementos da pilha e destrói a instância da pilha.
+         */
         ~Stack()
         {
             auto current = _first;
@@ -34,6 +54,11 @@ class Stack
             }
         }
 
+        /**
+         * @brief Insere um novo elemento no topo da pilha.
+         *
+         * @param data O elemento a ser inserido na pilha.
+         */
         void Insert(DataType data)
         {
             auto newNode = new Node<DataType>;
@@ -56,6 +81,12 @@ class Stack
             _size++;
         }
 
+        /**
+         * @brief Remove e retorna o elemento no topo da pilha.
+         *
+         * @return O elemento removido do topo da pilha.
+         * @throw empty_set_exception Se a pilha estiver vazia.
+         */
         DataType Remove()
         {
             if (_size == 0)
@@ -74,7 +105,22 @@ class Stack
 
             return data;
         }
-    
+
+        /**
+         * @brief Retorna o elemento no topo da pilha sem removê-lo.
+         *
+         * @return O elemento no topo da pilha.
+         */
+        DataType OnTop()
+        {
+            return _first->data;
+        }
+
+        /**
+         * @brief Verifica se a pilha está vazia.
+         *
+         * @return true se a pilha estiver vazia, false caso contrário.
+         */
         bool Empty()
         {
             return _size == 0;
